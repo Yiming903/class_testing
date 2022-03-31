d64ba07cea4ebfb28ea4e35006d4be844142c427
Yiming903 <102536224+Yiming903@users.noreply.github.com>
2022-03-30 22:27:25 -0500

    Created class example

--- class_1_testing.py
+++ class_1_testing.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 31 10:29:45 2021
+Spyder Editor
 
-@author: Jeff Levy
+This is a temporary script file.
+
+Created on March 30th
+
+@author: Yiming Zheng
 """
-
-print('Hello world!')
-
-print('No more math.')
+print('hello world')
+x = 10
+print(x*2)
